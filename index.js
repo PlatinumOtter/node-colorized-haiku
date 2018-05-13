899aa6e485a995d3f86a7ff11096835b74160f2a
Daniel Jackson <djackson3672@gmail.com>
2018-05-13 16:51:03 -0500

    updated for npm module use

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-var haiku = require('haiku-random');
-var color = require('cli-color');
+exports.printHaiku = function() {
+    var haiku = require('haiku-random');
+    var color = require('cli-color');
 
-console.log(color.red('Here is your Haiku:'))
-var thehaiku = haiku.random();
-console.log(color.cyan.bgBlack(thehaiku));
+    console.log(color.red('Here is your Haiku:'))
+    var thehaiku = haiku.random();
+    console.log(color.cyan.bgBlack(thehaiku));
+}
